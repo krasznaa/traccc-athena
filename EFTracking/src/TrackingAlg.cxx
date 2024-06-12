b7ad0cd176668035e4a9cea822148c6c1e69bd49
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2024-06-12 09:37:18 +0200

    Starting to add some code to EFTracking::TrackingAlg.

--- EFTracking/src/TrackingAlg.cxx
+++ EFTracking/src/TrackingAlg.cxx
@@ -1,9 +1,17 @@
 //
-// Copyright (C) 2023 CERN for the benefit of the ATLAS collaboration
+// Copyright (C) 2023-2024 CERN for the benefit of the ATLAS collaboration
 //
 
 // Local include(s).
 #include "TrackingAlg.h"
+
+// traccc include(s).
+#include <traccc/io/read_digitization_config.hpp>
+#include <traccc/io/read_geometry.hpp>
+#include <traccc/io/utils.hpp>
+
+// Detray include(s).
+#include <detray/io/frontend/detector_reader.hpp>
 
 DECLARE_COMPONENT(EFTracking::TrackingAlg)
 
@@ -12,6 +20,25 @@ namespace EFTracking
 
    StatusCode TrackingAlg::initialize()
    {
+      // Read in the geometry.
+      auto [surface_transforms, barcode_map] = traccc::io::read_geometry(
+          m_detectorFile, traccc::data_format::json);
+
+      // Set up the detector reader configuration.
+      detray::io::detector_reader_config cfg;
+      cfg.add_file(traccc::io::get_absolute_path(m_detectorFile));
+
+      // Read the detector.
+      auto det = detray::io::read_detector<host_detector_type>(m_resource, cfg);
+      m_hostDetector = std::move(det.first);
+
+      // Copy it to the device.
+      m_deviceDetector = detray::get_buffer(detray::get_data(m_hostDetector),
+                                            m_cuda_device_resource, m_cuda_copy);
+      m_cuda_stream.synchronize();
+
+      // Read the digitization configuration file
+      m_digitizationConfig = traccc::io::read_digitization_config(m_digitizationFile);
 
       // Return gracefully.
       return StatusCode::SUCCESS;
--- EFTracking/src/TrackingAlg.h
+++ EFTracking/src/TrackingAlg.h
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2023 CERN for the benefit of the ATLAS collaboration
+// Copyright (C) 2023-2024 CERN for the benefit of the ATLAS collaboration
 //
 #ifndef EFTRACKING_TRACKINGALG_H
 #define EFTRACKING_TRACKINGALG_H
@@ -9,7 +9,13 @@
 
 // Acts R&D include(s).
 #include <vecmem/memory/host_memory_resource.hpp>
+#include <vecmem/memory/cuda/device_memory_resource.hpp>
+#include <vecmem/memory/cuda/host_memory_resource.hpp>
+#include <vecmem/utils/cuda/copy.hpp>
+#include <detray/core/detector.hpp>
 #include <traccc/clusterization/clusterization_algorithm.hpp>
+#include <traccc/cuda/clusterization/clusterization_algorithm.hpp>
+#include <traccc/io/digitization_config.hpp>
 
 namespace EFTracking
 {
@@ -32,10 +38,61 @@ namespace EFTracking
       /// @}
 
    private:
+      /// @name Configuration options.
+      /// @{
+
+      /// The configuration options for the detector.
+      Gaudi::Property<std::string> m_detectorFile{
+          this, "DetectorFile", "geometries/odd/odd-detray_geometry_detray.json",
+          "The detector file to use"};
+      /// The detector digitization configuration file.
+      Gaudi::Property<std::string> m_digitizationFile{
+          this, "DigitizationFile", "geometries/odd/odd-digi-geometric-config.json",
+          "The digitization configuration file to use"};
+
+      /// @}
+
+      /// @name Acts R&D objects.
+      /// @{
+
+      /// Detector type used in host code
+      using host_detector_type = detray::detector<detray::default_metadata,
+                                                  detray::host_container_types>;
+      /// Detector type used in device code
+      using device_detector_type =
+          detray::detector<detray::default_metadata,
+                           detray::device_container_types>;
+
       /// Host memory resource to be used by the clusterization "algorithm"
       vecmem::host_memory_resource m_resource;
       /// The clusterization "algorithm"
-      traccc::clusterization_algorithm m_alg{m_resource};
+      traccc::host::clusterization_algorithm m_alg{m_resource};
+
+      /// CUDA stream to execute asynchronous clusterization on
+      traccc::cuda::stream m_cuda_stream;
+      /// CUDA pinned host memory resource
+      vecmem::cuda::host_memory_resource m_cuda_host_resource;
+      /// CUDA device memory resource
+      vecmem::cuda::device_memory_resource m_cuda_device_resource;
+      /// CUDA copy helper object
+      vecmem::cuda::copy m_cuda_copy;
+
+      /// Host detector object
+      host_detector_type m_hostDetector{m_resource};
+      /// Device detector buffer
+      host_detector_type::buffer_type m_deviceDetector;
+
+      /// Digitization configuration for the detector
+      traccc::digitization_config m_digitizationConfig;
+
+      /// The CUDA asynchronous clusterization "algorithm"
+      traccc::cuda::clusterization_algorithm
+          m_cuda_clusterization{{m_cuda_device_resource, &m_cuda_host_resource},
+                                m_cuda_copy,
+                                m_cuda_stream,
+                                1024};
+
+      /// @}
 
    }; // class TrackingAlg
 
